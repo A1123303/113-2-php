5846e82dd0a28fbc9e67c89bc4110b62d04ceb23
邱佑芹 <a1123303@mail.nuk.edu.tw>
2025-05-02 01:02:55 +0800

    Add files via upload

---
+++ HW_0425/register.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-05-01 19:02:37
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `register`
+--
+
+CREATE TABLE `register` (
+  `no` int(10) NOT NULL,
+  `name` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `photo` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- 傾印資料表的資料 `register`
+--
+
+INSERT INTO `register` (`no`, `name`, `email`, `photo`) VALUES
+(7, 'Ariel', 'a1123303@mail.nuk.edu.tw', 'https://a1123303.github.io/113-2-php/HW_0425/picture/Ariel_photo.png');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `register`
+--
+ALTER TABLE `register`
+  MODIFY `no` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
